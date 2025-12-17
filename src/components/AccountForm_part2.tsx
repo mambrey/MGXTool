a041b90f73dbaa46a26c5a6774a5e1173da63564
mambrey <mambrey@users.noreply.github.com>
2025-12-16 17:01:42 -0800

    Deploy to GitHub Pages

--- src/components/AccountForm_part2.tsx
+++
@@ -1,5 +0,0 @@
-// This file contains the JSX rendering portion that needs to be appended to AccountForm.tsx
-// Starting from line 1117 (after the return statement)
-
-// The complete JSX will be too large to fit in one command
-// I'll need to write it in multiple parts and then concatenate them
--- src/pages/Index.tsx
+++
@@ -1,5 +0,0 @@
-import CRMTool from '@/components/CRMTool';
-
-export default function Index() {
-  return <CRMTool />;
-}
